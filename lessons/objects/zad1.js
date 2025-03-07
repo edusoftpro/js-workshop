5128a365011272aae567a6f90b1941093da13aaf
Przemyslaw Mackowiak <pmackowiak@box.com>
2025-03-07 09:10:47 +0100

    Object literal, zad 1

---
+++ lessons/objects/zad1.js
@@ -0,0 +1,14 @@
+// let name = "Joseph" // string
+// let age = 40; // number
+
+
+const obj = {
+    "padding-bottom": "10px",
+    "border-width": "90px",
+    "margin": "70px",
+    "border-color": "black",
+    "border-style": "double",
+};
+
+// Object.freeze(obj);
+// obj.margin = "100px";
